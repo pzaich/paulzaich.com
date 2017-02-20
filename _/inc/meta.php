07eaf72f43da2ca2ce2fb0b0877dddd572e214ba
Paul Zaich <pzaich@gmail.com>
2017-02-19 21:41:52 -0800

    rm v1

--- _/inc/meta.php
+++
@@ -1,8 +0,0 @@
-<section class="meta">
-	<time datetime="<?php echo date(DATE_W3C); ?>" pubdate class="updated"><?php the_time('F jS, Y') ?></time>
-	<span class="byline author vcard">
-		<!--<i>by</i> <span class="fn"><?php //the_author() ?></span>-->
-	</span>
-	<?php sfc_like_button(); ?>
-	<?php //comments_popup_link('No Comments', '1 Comment', '% Comments', 'comments-link', ''); ?>
-</section>
